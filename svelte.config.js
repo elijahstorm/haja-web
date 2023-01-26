c2da3775ac798f665af43d4fad95c3a8bf8e9ae4
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-01-25 19:47:00 -0600

    remove base for standalone deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from "@sveltejs/adapter-auto"
 import preprocess from "svelte-preprocess"
 
-const dev = process.env.NODE_ENV === "development"
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
@@ -13,10 +11,7 @@ const config = {
 			hostineSite: "data",
 			precompress: false,
 			fallback: "index.html"
-		}),
-		paths: {
-			base: dev ? "" : "/haja-web"
-		}
+		})
 	}
 }
 

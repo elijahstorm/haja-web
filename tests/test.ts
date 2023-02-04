c49dc1abaec408cd78aefb406b118f64f45a1fcd
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-02-04 17:47:32 -0600

    add playwright

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test"
 
-test('about page has expected h1', async ({ page }) => {
-	await page.goto('/about');
-	await expect(page.locator('h1')).toHaveText('About this app');
-});
+test("should allow link to make new account", async ({ page }) => {
+	await page.goto("/team")
+	await expect(page.locator("span")).toHaveText("New to Haja?")
+})

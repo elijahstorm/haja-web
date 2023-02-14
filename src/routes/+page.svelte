4bcbdd472d2d44f6d7d2796d511ef968216e735d
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-02-14 21:10:38 +0900

    Improve homepage with recommended friends list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,21 @@
 	import { base } from "$app/paths"
 	import { getUser } from "$lib/Components/Content/User/UserContent"
 	import UserContent from "$lib/Components/Content/User/UserContent.svelte"
-	import HorizontalUserCard from "$lib/Components/Content/User/HorizontalUserCard.svelte"
 	import ContentWithFooter from "$lib/Components/PageContainers/ContentWithFooter.svelte"
 	import LandingPage from "$lib/Components/PageContainers/LandingPage.svelte"
 	import Loader from "$lib/Components/Widgets/Helpers/Loader.svelte"
 	import session from "$lib/firebase/session"
 	import Casing from "$lib/Components/PageContainers/Casing.svelte"
 	import Header from "$lib/Components/Branding/Header.svelte"
+	import ListWithActionAndTitle from "$lib/Components/Widgets/Layouts/ListWithActionAndTitle.svelte"
+	import HorizontalUserCard from "$lib/Components/Content/User/HorizontalUserCard.svelte"
 	import { getRecommendedUsers } from "$lib/Components/Content/User/SearchUsers"
-
-	$: myId = $session?.user?.uid
 </script>
 
 <ContentWithFooter>
-	{#if !myId}
-		<Header />
-		<LandingPage />
-	{:else}
+	{#if $session?.user?.uid}
 		<Casing>
-			{#await getUser({ id: myId })}
+			{#await getUser({ id: $session?.user?.uid })}
 				<Loader />
 			{:then user}
 				{#if typeof user === "string"}
@@ -34,16 +30,25 @@
 							My Teams
 						</a>
 					</UserContent>
+
+					<hr class="w-1/2 mx-auto my-12" />
+
+					<ListWithActionAndTitle title="Recommended Friends">
+						<div class="space-y-3">
+							{#await getRecommendedUsers( { exclude: [user.id] } ) then recommendedUsers}
+								{#each recommendedUsers as user}
+									{#if typeof user !== "string"}
+										<HorizontalUserCard {user} />
+									{/if}
+								{/each}
+							{/await}
+						</div>
+					</ListWithActionAndTitle>
 				{/if}
 			{/await}
 		</Casing>
+	{:else if $session?.ready}
+		<Header />
+		<LandingPage />
 	{/if}
 </ContentWithFooter>
-
-{#await getRecommendedUsers({}) then recommendedUsers}
-	{#each recommendedUsers as user}
-		{#if typeof user !== "string"}
-			<HorizontalUserCard {user} />
-		{/if}
-	{/each}
-{/await}

e4b4d240657791b572ef6add80f421ed3a927f21
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-10-16 07:07:05 +0700

    remove large browser list

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,4 @@
 import type { PlaywrightTestConfig } from "@playwright/test"
-import { devices } from "@playwright/test"
 
 const config: PlaywrightTestConfig = {
 	webServer: {
@@ -20,50 +19,6 @@ const config: PlaywrightTestConfig = {
 		baseURL: "http://localhost:3000",
 		trace: "on-first-retry",
 	},
-	projects: [
-		{
-			name: "chromium",
-			use: {
-				...devices["Desktop Chrome"],
-			},
-		},
-
-		{
-			name: "firefox",
-			use: {
-				...devices["Desktop Firefox"],
-			},
-		},
-
-		{
-			name: "webkit",
-			use: {
-				...devices["Desktop Safari"],
-			},
-		},
-
-		/* Test against mobile viewports. */
-		{
-			name: "Mobile Chrome",
-			use: {
-				...devices["Pixel 5"],
-			},
-		},
-		{
-			name: "Mobile Safari",
-			use: {
-				...devices["iPhone 12"],
-			},
-		},
-
-		/* Test against branded browsers. */
-		{
-			name: "Google Chrome",
-			use: {
-				channel: "chrome",
-			},
-		},
-	],
 	outputDir: "test-results/",
 }
 
